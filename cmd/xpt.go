e06f0e81691cfb31778bd419b7a76246bda2e6ce
Richard Jones <rjones@tangogroup.com>
2020-02-13 13:06:46 -0700

    moved some logic to separate function saveStackAsYml so it can be shared with other commands

--- cmd/xpt.go
+++ cmd/xpt.go
@@ -10,7 +10,6 @@ import (
 	"cuelang.org/go/cue/build"
 	"github.com/TangoGroup/stx/stx"
 	"github.com/ghodss/yaml"
-	"github.com/logrusorgru/aurora"
 	"github.com/spf13/cobra"
 )
 
@@ -20,25 +19,29 @@ var xptCmd = &cobra.Command{
 	Short: "eXPorTs cue templates that implement the Stacks:[] pattern.",
 	Long:  `Yada yada yada.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		au := aurora.NewAurora(true)
 		buildInstances := stx.GetBuildInstances(args, "cfn")
 		stx.Process(buildInstances, func(buildInstance *build.Instance, cueInstance *cue.Instance, cueValue cue.Value) {
 			stacks := stx.GetStacks(cueValue)
 			if stacks != nil {
 				for stackName, stack := range stacks {
-					dir := filepath.Clean(buildInstance.Root + "/../yml/cfn/" + stack.Profile)
-					os.MkdirAll(dir, 0766)
-					//fmt.Println(err)
-					fileName := dir + "/" + stackName + ".cfn.yml"
-					fmt.Printf("%s %s %s %s\n", au.White("Saving"), au.Magenta(stackName), au.White("⤏"), fileName)
-					template := cueValue.Lookup("Stacks", stackName, "Template")
-					yml, _ := yaml.Marshal(template)
-					//fmt.Printf("YAML: %+v\n", string(yml))
-					ioutil.WriteFile(fileName, yml, 0766)
+					saveStackAsYml(stackName, stack, buildInstance, cueValue)
 				}
 			}
 		})
 	},
+}
+
+func saveStackAsYml(stackName string, stack stx.Stack, buildInstance *build.Instance, cueValue cue.Value) string {
+	dir := filepath.Clean(buildInstance.Root + "/../yml/cfn/" + stack.Profile)
+	os.MkdirAll(dir, 0766)
+	//fmt.Println(err)
+	fileName := dir + "/" + stackName + ".cfn.yml"
+	fmt.Printf("%s %s %s %s\n", au.White("Saving"), au.Magenta(stackName), au.White("⤏"), fileName)
+	template := cueValue.Lookup("Stacks", stackName, "Template")
+	yml, _ := yaml.Marshal(template)
+	//fmt.Printf("YAML: %+v\n", string(yml))
+	ioutil.WriteFile(fileName, yml, 0766)
+	return fileName
 }
 
 func init() {
