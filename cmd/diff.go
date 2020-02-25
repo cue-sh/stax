1ac59144ab39b09347b4fbf81c7ea934ce833d6d
Joel Longtine <jlongtine@tangogroup.com>
2020-02-25 13:58:55 -0700

    Remove `continue` statements

--- cmd/diff.go
+++ cmd/diff.go
@@ -62,13 +62,11 @@ func diff(cfn *cloudformation.CloudFormation, stackName, templateBody string) {
 	})
 	if err != nil {
 		fmt.Printf("%+v\n", au.Red("Error getting template for stack: "+stackName))
-		continue
 	} else {
 		// fmt.Println(*existingTemplate.TemplateBody)
 		r, _ := regexp.Compile("!(Base64|Cidr|FindInMap|GetAtt|GetAZs|ImportValue|Join|Select|Split|Sub|Transform|Ref|And|Equals|If|Not|Or)")
 		if r.MatchString(*existingTemplate.TemplateBody) {
 			fmt.Printf("  %+v\n", au.Red("The existing stack uses short intrinsic functions, unable to create diff: "+stackName))
-			continue
 		} else {
 			existingDoc, _ := ytbx.LoadDocuments([]byte(*existingTemplate.TemplateBody))
 			doc, _ := ytbx.LoadDocuments([]byte(templateBody))
@@ -78,7 +76,6 @@ func diff(cfn *cloudformation.CloudFormation, stackName, templateBody string) {
 			)
 			if err != nil {
 				fmt.Printf("%+v\n", au.Red("Error creating template diff for stack: "+stackName))
-				continue
 			} else {
 				reportWriter := &dyff.HumanReport{
 					Report:     report,
