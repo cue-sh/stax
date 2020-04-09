d01bde41ce3498d14134572c4530cd78a886777f
Richard Jones <rjones@tangogroup.com>
2020-04-09 11:41:18 -0600

    import is working!

--- cmd/import.go
+++ cmd/import.go
@@ -1,18 +1,20 @@
 package cmd
 
 import (
-	"cuelang.org/go/cue"
-	"cuelang.org/go/cue/build"
-	"cuelang.org/go/cue/format"
 	"cuelang.org/go/encoding/json"
 	"github.com/TangoGroup/stx/stx"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
+	"github.com/ghodss/yaml"
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	rootCmd.AddCommand(importCmd)
+	importCmd.Flags().StringVar(&flags.ImportStack, "stack", "", "Stack name to import. (Required)")
+	importCmd.MarkFlagRequired("stack")
+	importCmd.Flags().StringVar(&flags.ImportRegion, "region", "", "Region where stack is located. (Required)")
+	importCmd.MarkFlagRequired("region")
 }
 
 // importCmd represents the import command
@@ -23,61 +25,44 @@ var importCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 
 		defer log.Flush()
+
+		if flags.Profile == "" {
+			log.Error("--profile is required for import")
+		}
+
+		flags.StackNameRegexPattern = "^" + flags.ImportStack + "$"
+
 		stx.EnsureVaultSession(config)
 
-		buildInstances := stx.GetBuildInstances(args, "cfn")
+		// get a session and cloudformation service client
+		session := stx.GetSession(flags.Profile)
+		cfn := cloudformation.New(session, aws.NewConfig().WithRegion(flags.ImportRegion))
+		log.Infof("%s %s...", au.White("Importing"), au.Magenta(flags.ImportStack))
 
-		stx.Process(buildInstances, flags, log, func(buildInstance *build.Instance, cueInstance *cue.Instance) {
-			stacksIterator, stacksIteratorErr := stx.NewStacksIterator(cueInstance, flags, log)
-			if stacksIteratorErr != nil {
-				log.Fatal(stacksIteratorErr)
+		getTemplateOutput, getTemplateErr := cfn.GetTemplate(&cloudformation.GetTemplateInput{StackName: aws.String(flags.ImportStack), TemplateStage: aws.String("Processed")})
+
+		if getTemplateErr != nil {
+			log.Error(getTemplateErr)
+			return
+		}
+
+		templateBytes := []byte(aws.StringValue(getTemplateOutput.TemplateBody))
+
+		if !json.Valid(templateBytes) {
+			// it must be yaml so convert to json
+			var yamlErr error
+			templateBytes, yamlErr = yaml.YAMLToJSON(templateBytes)
+			if yamlErr != nil {
+				log.Error(yamlErr)
+				return
 			}
+		}
 
-			for stacksIterator.Next() {
-				stackValue := stacksIterator.Value()
-				var stack stx.Stack
-				decodeErr := stackValue.Decode(&stack)
-				if decodeErr != nil {
-					log.Error(decodeErr)
-					continue
-				}
-
-				// get a session and cloudformation service client
-				session := stx.GetSession(stack.Profile)
-				cfn := cloudformation.New(session, aws.NewConfig().WithRegion(stack.Region))
-				log.Infof("%s %s...", au.White("Importing"), au.Magenta(stack.Name))
-
-				getTemplateOutput, getTemplateErr := cfn.GetTemplate(&cloudformation.GetTemplateInput{StackName: aws.String(stack.Name), TemplateStage: aws.String("Processed")})
-
-				if getTemplateErr != nil {
-					log.Error(getTemplateErr)
-					continue
-				}
-				log.Check()
-				log.Infof("%s\n", aws.StringValue(getTemplateOutput.TemplateBody))
-				jsonBytes := []byte(aws.StringValue(getTemplateOutput.TemplateBody))
-				expression, extractErr := json.Extract("", jsonBytes)
-				if extractErr != nil {
-					log.Error(extractErr)
-					continue
-				}
-				log.Infof("%+v\n", expression)
-
-				newCueInstance, fillErr := cueInstance.Fill(expression, "Stacks", stack.Name, "Template")
-
-				if fillErr != nil {
-					log.Error(fillErr)
-					continue
-				}
-
-				result, formatErr := format.Node(newCueInstance.Value().Syntax(), format.Simplify())
-				if formatErr != nil {
-					log.Error(formatErr)
-					continue
-				}
-				log.Infof("%s\n", result)
-			}
-		})
+		templateErr := createTemplate(args, string(templateBytes))
+		if templateErr != nil {
+			log.Error(templateErr)
+		}
+		log.Check()
 	},
 }
 
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/TangoGroup/stx
 go 1.13
 
 require (
-	cuelang.org/go v0.0.15
+	cuelang.org/go v0.0.16-0.20200409135714-882d88d6bbd8
 	github.com/aws/aws-sdk-go v1.29.2
 	github.com/briandowns/spinner v1.8.0
 	github.com/deckarep/golang-set v1.7.1
@@ -14,6 +14,6 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381
 	github.com/olekukonko/tablewriter v0.0.4
-	github.com/spf13/cobra v0.0.5
+	github.com/spf13/cobra v0.0.7
 	go.mozilla.org/sops/v3 v3.5.0
 )
