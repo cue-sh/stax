f0943cd207c1f5cefc507c63812055d3398e66ee
Christopher Montes <2421856+lodotek@users.noreply.github.com>
2020-08-25 15:07:29 -0600

    Improve confirmation prompt #59

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -425,7 +425,7 @@ func deployStack(stack stx.Stack, buildInstance *build.Instance, stackValue cue.
 
 	diff(cfn, stack.Name, templateBody)
 
-	log.Infof("%s %s\n▶︎", au.BrightBlue("Execute change set?"), "Y to execute. Anything else to cancel.")
+	log.Infof("%s %s %s %s\n▶︎", au.White("Execute change set "), au.BrightBlue(changeSetName), au.White("?"), "Y to execute. Anything else to cancel.")
 	var input string
 	fmt.Scanln(&input)
 
