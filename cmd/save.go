996bd4bb2b18943c381c56db2178b78bcffa480d
Richard <the@talentedmrjones.com>
2021-04-23 11:49:42 -0600

    refactored to save outputs colocated locally with the stack rather than in cue.mod/user/cfn.out

--- cmd/save.go
+++ cmd/save.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"cuelang.org/go/cue"
 	"cuelang.org/go/cue/build"
@@ -94,35 +92,12 @@ func saveStackOutputs(buildInstance *build.Instance, stack stx.Stack) error {
 		return nil
 	}
 
-	// cfn.out files are store under cue.mod/usr/cfn.out with the same relative path as the stacks cue instance
-	// for example a stack with a template declared in cue/engineering/eks/cluster
-	// with a concrete leaf in cue/engineering/eks/cluster/dev-usw2
-	// would store outputs in cue.mod/usr/cfn.out/cue/engineering/eks/cluster
-	instancePath := buildInstance.Dir
-	// in case no template.cfn.cue file is found, use the instance (relative) path
-	cueOutPath := strings.Replace(instancePath, buildInstance.Root, "", 1)
-
-	// look for the template.cfn.cue file for the current build instance
-	dirs := strings.Split(instancePath, config.OsSeparator)
-	path := ""
-	// traverse the directory tree starting from leaf going up to successive parents
-	for i := len(dirs); i > 0; i-- {
-		path = strings.Join(dirs[:i], config.OsSeparator)
-		// look for the template file
-		if _, err := os.Stat(path + config.OsSeparator + "template.cfn.cue"); !os.IsNotExist(err) {
-			break // found it!
-		}
-	}
-	if path != "" {
-		cueOutPath = strings.Replace(path, buildInstance.Root, "", 1)
-	}
-	cueOutPath = strings.Replace(buildInstance.Root+"/cue.mod/usr/cfn.out"+cueOutPath, "-", "", -1)
-	fileName := cueOutPath + "/" + stack.Name + ".out.cue"
+	cueOutPath := buildInstance.Dir + "/cfnout/"
+	fileName := cueOutPath + stack.Name + ".out.cue"
 	log.Infof("%s %s %s %s\n", au.White("Saving"), au.Magenta(stack.Name), au.White("⤏"), fileName)
 
 	// create the .out.cue file
-	cuePackage := filepath.Base(cueOutPath)
-	result := "package " + cuePackage + "\n\n\"" + stack.Name + "\": {\n"
+	result := "package cfnout" + "\n\n\"" + stack.Name + "\": {\n"
 	// convert cloudformation outputs into simple key:value pairs
 	for _, output := range describeStacksOutput.Stacks[0].Outputs {
 		result += fmt.Sprintf("\"%s\":\"%s\"\n", aws.StringValue(output.OutputKey), aws.StringValue(output.OutputValue))
@@ -138,7 +113,7 @@ func saveStackOutputs(buildInstance *build.Instance, stack stx.Stack) error {
 
 	// save it!
 	os.MkdirAll(cueOutPath, 0766)
-	writeErr := ioutil.WriteFile(fileName, cueOutput, 0644)
+	writeErr := ioutil.WriteFile(fileName, []byte(cueOutput), 0644)
 	if writeErr != nil {
 		return writeErr
 	}
